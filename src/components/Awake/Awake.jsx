4e1730c331c86b2791eb2b1fd58ad7d5d318eba6
Mostafa Wahied <97263286+Mostafa-Wahied@users.noreply.github.com>
2023-08-21 09:44:32 -0700

    handling error in Awake.jsx

--- src/components/Awake/Awake.jsx
+++ src/components/Awake/Awake.jsx
@@ -1,34 +1,38 @@
 import { useEffect } from 'react';
 
-const fetchProjectsData = () => {
-    const project1URL = 'https://takenote-euho.onrender.com/';
-    const project2URL = 'https://mastermind-webapp.onrender.com/';
+const fetchProjectsData = async () => {
+  try {
+    const response1 = await fetch(project1URL, {mode: 'no-cors'});
+    if(response1.ok) {
+      console.log('Fetched data from TakeNote');
+    } else {
+      throw new Error('Failed to fetch data from TakeNote'); 
+    }
 
-    fetch(project1URL, { mode: 'no-cors' })
-        .then((response) => {
-            if (response.type === 'opaque') {
-                console.log('Fetched data from TakeNote');
-            }
-        });
+    const response2 = await fetch(project2URL, {mode: 'no-cors'});
+    if(response2.ok) {
+      console.log('Fetched data from Mastermind');
+    } else {
+      throw new Error('Failed to fetch data from Mastermind');
+    }
 
-    fetch(project2URL, { mode: 'no-cors' })
-        .then((response) => {
-            if (response.type === 'opaque') {
-                console.log('Fetched data from Mastermind');
-            }
-        });
-};
+  } catch (error) {
+    console.error(error);
+  }
+}
 
 export default function Awake() {
-    useEffect(() => {
-        fetchProjectsData(); // Fetch data immediately when the component mounts
 
-        const intervalId = setInterval(() => {
-            fetchProjectsData();
-        }, 840000); // 14 minutes
+  useEffect(() => {
+    fetchProjectsData();
 
-        return () => clearInterval(intervalId); // Clean up the interval when the component unmounts
-    }, []);
+    const intervalId = setInterval(() => {
+      fetchProjectsData(); 
+    }, 840000); // 14 minutes
 
-    return null;
+    return () => clearInterval(intervalId); // clean the interval after the component unmounts
+
+  }, []);
+
+  return null;
 }
